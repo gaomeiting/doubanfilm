a60f7025703fec8b5e721f8143fc080fd73ca2dd
gaomeiting <1959151877@qq.com>
2018-05-02 16:38:10 +0800

    分页搜索

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,4 +1,57 @@
 const app=getApp();
 Page({
-	
+	data: {
+		inputValue: "",
+		currentPage: 1,
+		more: false,
+		movies: []
+	},
+	search(e) {
+		const value = e.detail.value;
+		this.setData({
+			currentPage: 1,
+			more: true,
+			inputValue: value
+		})
+		app.douban.find("search", this.currentPage, 10, value).then(res => {
+			const movies=this.data.movies.concat(res.subjects)
+			this.setData({
+				movies
+			})
+			this._hasMore(res.start, res.count, res.total)
+		})
+	},
+	pullUp() {
+		console.log("more", this.data.more)
+		if(!this.data.more) return;
+		const currentPage=this.data.currentPage+1;
+		console.log(currentPage)
+		this.setData({
+			currentPage
+		})
+		const value = this.data.inputValue;
+		app.douban.find("search", this.data.currentPage, 10, value).then(res => {
+			const movies=this.data.movies.concat(res.subjects)
+			this.setData({
+				movies
+			})
+			/*console.log(res.start+res.count<res.total)*/
+			this._hasMore(res.start, res.count, res.total)
+		})
+	},
+	_hasMore(start, count, total) {
+		console.log(start+count<total, this.data.more)
+		if(start+count < total) {
+			this.setData({
+				more: true
+			})
+		}
+		else {
+			this.setData({
+				more: false
+			})
+		}
+	}
+
+
 })
